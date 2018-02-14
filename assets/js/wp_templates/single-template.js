894f1bdd99444d9c580074381e4d451da79a8ca1
eresyyl <eresyyl@gmail.com>
2018-02-14 17:52:16 +0700

    Adding script to change blocks name and fix css

--- assets/js/wp_templates/single-template.js
+++ assets/js/wp_templates/single-template.js
@@ -2,13 +2,20 @@
 	'use strict';
 	const searchString = "Title";
 
-	$('[data-event="add-layout"]').remove();
-	$('[data-event="remove-layout"]').remove();
-	$('[data-name="total_slug"]')[0].remove();
-	
-	$(`div.acf-fields > [data-name="${searchString.toLowerCase()}"]  label:contains("${searchString}")`).each( (index, label) => {
-		if( label.innerHTML != searchString || !label.attributes.for) return;
+	$(window).load( () => {
+		$('[data-event="add-layout"]').remove();
+		$('[data-event="remove-layout"]').remove();
+		$('[data-name="total_slug"]')[0].remove();
 		
-		$(`#${label.attributes.for.value}`).prop('readonly', true);
+		$(`div.acf-fields > [data-name="${searchString.toLowerCase()}"]  label:contains("${searchString}")`).each( (index, label) => {
+			if( label.innerHTML != searchString || !label.attributes.for) return;
+			
+			let title = $(`#${label.attributes.for.value}`);
+			let block_title = $(label.closest('.layout')).find('.acf-fc-layout-handle');
+			let block_number = block_title.find('.acf-fc-layout-order');
+			if(block_number[0]) block_title.html(`${block_number.outerHTML()} ${title.val()}`);
+			
+			title.prop('readonly', true);
+		});
 	});
 })(window, document, jQuery);
