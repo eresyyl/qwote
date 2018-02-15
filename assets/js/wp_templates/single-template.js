557dacbc4c5b4ba196699e4fd2983087e8f9e6ed
eresyyl <eresyyl@gmail.com>
2018-02-15 15:29:26 +0700

    Removing slug, type of fields and type of price

--- assets/js/wp_templates/single-template.js
+++ assets/js/wp_templates/single-template.js
@@ -1,14 +1,15 @@
 (function(window, document, $) {
 	'use strict';
-	const searchString = "Title";
+	const readonlyField = "Title",
+		removingFields = ["slug", "type_of_fields", "type_of_price"];
 
 	$(window).load( () => {
 		$('[data-event="add-layout"]').remove();
 		$('[data-event="remove-layout"]').remove();
 		$('[data-name="total_slug"]')[0].remove();
 		
-		$(`div.acf-fields > [data-name="${searchString.toLowerCase()}"]  label:contains("${searchString}")`).each( (index, label) => {
-			if( label.innerHTML != searchString || !label.attributes.for) return;
+		$(`div.acf-fields > [data-name="${readonlyField.toLowerCase()}"]  label:contains("${readonlyField}")`).each( (index, label) => {
+			if( label.innerHTML != readonlyField || !label.attributes.for) return;
 			
 			let title = $(`#${label.attributes.for.value}`);
 			let block_title = $(label.closest('.layout')).find('.acf-fc-layout-handle');
@@ -17,5 +18,9 @@
 			
 			title.prop('readonly', true);
 		});
+		
+		for(let i in removingFields){
+			$(`div.acf-fields > .acf-field[data-name="${removingFields[i]}"]`).remove();
+		}
 	});
 })(window, document, jQuery);
